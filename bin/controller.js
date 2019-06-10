8376d88fbff26bace82d208964fd4f010c66e5b7
jeancarlos006 <jeancarloscorderohuertasfcb@gmail.com>
2019-06-10 19:54:37 +0000

    hola9

--- bin/controller.js
+++ bin/controller.js
@@ -4,32 +4,33 @@ const User = require("./models/User");
 class controller{
     constructor (){
         this.connect();
-}
-async connect(){
+    }
+    async connect(){
 
-try{
-await mongoose.connect(
-    "mongodb+srv://jeancarlos006:jeancarlos16fcb.@cluster0-wzhie.mongodb.net/SISTEMA?retryWrites=true&w=majority",
-    {useNewUrlParser:true}
-);
-console.log('conectados a la base de datos!')
-}catch(e){
-    console.error(e)
-}
-}
-getUsers (res){
-    User.find({},(err,users)=>{
-        if(err) throw err;
-        res.send(users);
-    })
-}
-}
-postUsers(req, res){
-    let user = req.boddy.users;
-    User.create(user,(err, newUser)=>{
-        if(err) throw err;
-        res.send({nU:newUser})
-    })
-}
+    try{    
+    await mongoose.connect(
+        "mongodb+srv://jeancarlos006:jeancarlos16fcb.@cluster0-wzhie.mongodb.net/SISTEMA?retryWrites=true&w=majority",
+                    {useNewUrlParser:true}
+                );
+    console.log('conectados a la base de datos!')
+    }catch(e){
+        console.error(e)
+    }
+    }
+    getUsers (res){
+        User.find({},(err,users)=>{
+            if(err) throw err;
+            res.send(users);
+        })
+    }
+    postUsers(req, res){
+        let user = req.boddy.users;
+        User.create(user,(err, newUser)=>{
+            if(err) throw err;
+            res.send({nU:newUser})
+        })
+    }
 
-exports.controller = new controller()
+    }
+
+    exports.controller = new controller()
