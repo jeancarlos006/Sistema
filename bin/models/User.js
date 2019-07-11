c8881e1c4630256b124c22bd8603f873acbc97ad
jeancarlos006 <jeancarloscorderohuertasfcb@gmail.com>
2019-07-11 20:32:48 +0000

    hola36

--- bin/models/User.js
+++ bin/models/User.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const usuarioSchema = new Schema ({
+const UserSchema = new Schema ({
  nickname: String,
  password: String,
  picture: String,
 });
 
-var usuario=  mongoose.model("usuarioSchema",UserSchema);
-module.exports = usuario;
+var User=  mongoose.model("User",UserSchema);
+module.exports = User;
